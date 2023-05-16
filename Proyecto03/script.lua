e096cdee3f5a3f88d09f1cdb0647b24b0566e18b
sr-misa <m.ramirezramirez7@ugto.mx>
2023-05-15 19:47:29 -0600

    quitar cosas inutiles en script

--- Proyecto03/script.lua
+++ Proyecto03/script.lua
@@ -1,14 +1,13 @@
 function sysCall_init()
-    print('init')
+    
 end
 
 function sysCall_actuation()
-    -- put your actuation code here
-    print('actuation')
+    
 end
 
 function sysCall_sensing()
-    print('sensing')
+    
 end
 
 function getMap(size, scala, obj)
@@ -27,6 +26,5 @@ function getMap(size, scala, obj)
 end
 
 function sysCall_cleanup()
-print('end')
-    -- do some clean-up here
+
 end
